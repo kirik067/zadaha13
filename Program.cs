a610af43086f26657f1c4f19ca91bebbf6ea9b63
kirill <tvoag.2015@mail.ru>
2022-11-20 16:17:44 +0500

    задача13

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Видите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99)
+{
+Console.WriteLine($"третья цифра: {num = (num % 10) % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
