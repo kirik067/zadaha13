8679c5066709032a7fa79a7110c8be00d9575ee4
kirill <tvoag.2015@mail.ru>
2022-11-23 18:33:27 +0500

    решил задачю правельно!!!

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿Console.WriteLine("Видите трёхзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num > 99)
-{
-    Console.WriteLine($"третья цифра: {num = (num / 100) % 10}");
-}
-else
+if (num <= 99)
 {
     Console.WriteLine("Третьей цифры нет");
+}
+else
+{ 
+    while (num > 999)
+    {
+    num /= 10;
+    }
+    Console.WriteLine(num % 10);
 }
