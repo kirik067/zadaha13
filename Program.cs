d7d6f23e223901c23d32080e3e5bc714642b9bb6
kirill <tvoag.2015@mail.ru>
2022-11-30 18:22:58 +0500

    25 27 29

--- Program.cs
+++ Program.cs
@@ -1,15 +1,53 @@
-﻿Console.WriteLine("Видите трёхзначное число");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
-if (num <= 99)
-{
-    Console.WriteLine("Третьей цифры нет");
-}
-else
-{ 
-    while (num > 999)
-    {
-    num /= 10;
-    }
-    Console.WriteLine(num % 10);
-}
+// int[] array = new int[8];
+// Console.WriteLine();
+// var arr = FillArray(8);
+// PrintArray(arr);
+
+// int[] FillArray(int length)
+// {
+//     int[] result = new int[length];
+//     for(int i =0; i < result.Length; i++)
+//     {
+//         result[i] = new Random().Next(111, 999 );   
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     for(int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]}, ");
+//     }
+//     for (int z = 0; z < arr.Length; z++)
+//      if (arr[z] % 2 == 0)
+
+//     Console.WriteLine($" {z} из них чётные");
+
+    
+
+// }
+
+
+
+
+
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+
+
+
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
